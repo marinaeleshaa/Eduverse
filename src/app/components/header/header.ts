c4f6037b0c98a74e4ebc522fedb4cdc2f18c8b74
Amrsamy19 <amrsamy622@gmail.com>
2025-10-19 17:15:33 +0300

    feat: profile menu

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
 export class Header {
+  profileMenuOpen: boolean;
 
+  constructor() {
+    this.profileMenuOpen = false;
+  }
+
+  toggleProfileMenu(event: Event) {
+    event.stopPropagation();
+    this.profileMenuOpen = !this.profileMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeProfileMenu(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const clickedInside = target.dataset['profileMenu'] === 'true';
+    if (!clickedInside) {
+      this.profileMenuOpen = false;
+    }
+  }
 }
