0a0beb4846100140fd4b944be8396809895a046e
marina eleshaa <marinaeleshaa>
2025-10-24 22:05:00 +0300

    responsiveness of about page done

--- src/app/components/about-page/about-page.ts
+++ src/app/components/about-page/about-page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { About } from '../../about/about';
+import { HeadSection } from "../head-section/head-section";
+import { OurMissionComponent } from '../about sections/our-mission/our-mission.component';
+import { OurStoryComponent } from '../about sections/our-story/our-story.component';
+import { OurTeamComponent } from '../about sections/our-team/our-team.component';
+import { WhyChooseUsComponent } from '../about sections/why-choose-us/why-choose-us.component';
+import { OurValuesComponent } from '../about sections/our-values/our-values.component';
+import { Our } from '../about sections/our/our';
+
 
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [CommonModule, About],
+  imports: [CommonModule, HeadSection ,OurMissionComponent, OurStoryComponent, OurTeamComponent, WhyChooseUsComponent, Our, OurValuesComponent],
+
+
   templateUrl: './about-page.html',
   styleUrl: './about-page.css',
 })
