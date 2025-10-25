b30795624ed501b9ada35500e416ac2fdd8fc241
marina eleshaa <marinaeleshaa>
2025-10-25 15:01:46 +0300

    add guards

--- src/app/guards/login-protected-guard.ts
+++ src/app/guards/login-protected-guard.ts
@@ -2,14 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserAuth } from '../services/user-auth';
 
-export const loginAuthGuard: CanActivateFn = (route, state) => {
+export const loginProtectedGuard: CanActivateFn = (route, state) => {
   const userAuth = inject(UserAuth);
   const router = inject(Router);
 
-  console.log(userAuth.isLoggedIn);
-
-  if (!userAuth.isLoggedIn) {
-    router.navigate(['/login']);
+  if (userAuth.isLoggedIn && (state.url === '/login' || state.url === '/signUp')) {
+    router.navigate(['/home']);
     return false;
   }
 
