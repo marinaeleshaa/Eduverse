10f8425e01d98307f22fe07f0336a3fe7d1a0d0a
marina eleshaa <marinaeleshaa>
2025-10-26 13:42:52 +0300

    add btn in about page

--- src/app/components/about-page/about sections/why-choose-us/why-choose-us.component.ts
+++ src/app/components/about-page/about sections/why-choose-us/why-choose-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-why-choose-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './why-choose-us.component.html',
   styleUrl: './why-choose-us.component.css',
 })
