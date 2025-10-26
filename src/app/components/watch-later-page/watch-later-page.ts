c0b299bda2c891f5cc4a859de00c8d8c9f4594a4
marwan <marwanabdalmagied@gmail.com>
2025-10-26 20:53:37 +0300

    add watch later page

---
+++ src/app/components/watch-later-page/watch-later-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeadSection } from "../head-section/head-section";
+
+@Component({
+  selector: 'app-watch-later-page',
+  imports: [HeadSection],
+  templateUrl: './watch-later-page.html',
+  styles: ``
+})
+export class WatchLaterPage {
+
+}
