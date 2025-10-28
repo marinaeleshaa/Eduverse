c13d77690c901dbead738577a483e9ed60680dec
Amrsamy19 <amrsamy622@gmail.com>
2025-10-29 00:55:53 +0300

    feat: show courses dynamically

--- src/app/components/courses/courses-page/courses-page.ts
+++ src/app/components/courses/courses-page/courses-page.ts
@@ -1,58 +1,36 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-interface Course {
-  id: number;
-  name: string;
-  category: string;
-  imageSrc: string;
-}
+import { CourseService } from '../../../services/course-service';
+import { ICourse } from '../../../Interfaces/icourse';
+import { Observable, map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-courses-page',
-  imports: [RouterLink,FormsModule],
+  imports: [RouterLink, FormsModule, AsyncPipe],
   templateUrl: './courses-page.html',
-  styleUrl: './courses-page.css'
+  styleUrl: './courses-page.css',
 })
 export class CoursesPage implements OnInit {
-   courses: Course[] = [
-    {
-      id: 101,
-      name: 'Advanced TypeScript & Signals',
-      category: 'Development',
-      imageSrc: 'https://placehold.co/600x400/312e81/ffffff?text=TS+Code',
-    },
-    {
-      id: 102,
-      name: 'Tailwind CSS for Angular Developers',
-      category: 'Design',
-      imageSrc: 'https://placehold.co/600x400/29007A/ffffff?text=Tailwind+UI',
-    },
-    {
-      id: 103,
-      name: 'Mastering Reactive Programming (RxJS)',
-      category: 'Architecture',
-      imageSrc: 'https://placehold.co/600x400/4c1d95/ffffff?text=RxJS+Streams',
-    },
-    {
-      id: 104,
-      name: 'Full-Stack with Node.js & NestJS',
-      category: 'Backend',
-      imageSrc: 'https://placehold.co/600x400/6d28d9/ffffff?text=Node+Database',
-    },
-  ];
+  courses!: Observable<ICourse[]>;
   searchTerm: string = '';
-  filteredCourses: Course[] = [];
+  filteredCourses!: Observable<ICourse[]>;
+
+  constructor(private courseService: CourseService) {}
 
   ngOnInit() {
-    this.filteredCourses = this.courses; // Initialize with all courses
+    // Initialize with all courses
+    this.courseService.getCourses();
+    this.courses = this.courseService.courses$;
+    this.filteredCourses = this.courses;
   }
 
   filterCourses() {
     const term = this.searchTerm.toLowerCase();
-    this.filteredCourses = this.courses.filter(course => 
-      course.name.toLowerCase().includes(term) || 
-      course.category.toLowerCase().includes(term)
+
+    // Filter the courses based on the search term
+    this.filteredCourses = this.courses.pipe(
+      map((courses) => courses.filter((course) => course.courseName.toLowerCase().includes(term)))
     );
   }
-
 }
